a7322e13b1f2d87db21dc34f515f41600025265e
wvves <erkin.t14@gmail.com>
2023-09-11 21:42:14 +0600

    add right panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React, { HtmlHTMLAttributes, useRef } from 'react'
 import './App.css'
 import Canvas from './components/Canvas'
+import LoginPage from './page/LoginPage'
 
 function App() {
-
+  
   return (
     <>
-    <div className='App'>
-      <Canvas size={450}></Canvas>
+    <div>tic tac toe</div>
+    <div className='Canvas-main'>
+      <Canvas size={400}></Canvas>
+      <div className='right-panel'>
+        <div>
+          <div>name</div>
+          <button>login</button>
+        </div>
+        <div>
+          <div>start game</div>
+          <div>score</div>
+          <div>0 : 0</div>
+        </div>
+
+      </div>
     </div>
+    {/* <LoginPage/> */}
     </>
   )
 }
