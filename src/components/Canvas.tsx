614d667fca088a925f8b427064a31b84ec73c827
wvves <erkin.t14@gmail.com>
2023-09-19 22:37:42 +0600

    add func win, draw

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,47 +1,48 @@
-import React, { MouseEventHandler, useEffect } from 'react';
-import Rectangle from './Reactangle';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import Modal from './Modal';
 
 interface CanvasProps {
   size: number
 }
 
-function matrixArray(arr: string[], length: number) {
-  const defArray = [...arr]
-  const resArray: Array<any> = []
-  for(let index = 0; index < length; index++) {
-    const rowArray: Array<string | undefined> = []
-    for (let jndex = 0; jndex < length; jndex++) {
-      rowArray[jndex] = defArray.shift()
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function checkWin(arr: string[]) {
+  for(const combination of winCombinations) {
+    const [a, b, c] = combination;
+    if(arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+      return true;
     }
-    resArray.push(rowArray)
   }
-  return resArray
+  return false
+}
+
+function checkDraw(arr: string[]) {
+  return [...arr].every((element) => element !== '')
 }
 
 const Canvas: React.FC<CanvasProps> = ({ size }) => {
-  // const canvasRefs: Array<React.RefObject<HTMLCanvasElement | null>> = Array(9).fill(null);
-  // const canvasRefs = React.useRef<(HTMLCanvasElement | null)[]>(Array(9).fill(null))
-  // const resArray: Array<Array<string | null>> = Array(3).fill(Array(3).fill(null))
-  // console.log(resArray)
   const canvasRefs = React.useRef<Array<HTMLCanvasElement | null>>([])
   const contexts = React.useRef<Array<CanvasRenderingContext2D | null>>([]);
   const widhtSize = size + 6
-  const [isTurn, setIsTurn] = React.useState(false)
-  const [value, setValue] = React.useState<number | undefined>()
-  const [pass, setPass] = React.useState<string[]>(Array(9).fill(null))
+  const [currentPlayer, setCurrentPlayer] = React.useState('x')
+  const [gameActive, setGameActive] = React.useState<boolean>(true)
+  const [pass] = React.useState<string[]>(Array(9).fill(''))
+  const [modalMessage, setModalMessage] = React.useState<string>('')
 
   React.useEffect(() => {
     canvasRefs.current.forEach((canvasRef, index) => {
       if (canvasRef) {
         contexts.current[index] = canvasRef.getContext('2d')
-        // if (context) {
-        //   contexts.current[index]!.strokeStyle = 'red';
-        //   contexts.current[index]?.beginPath();
-        //   contexts.current[index]?.rect(0, 0, 20, 40);
-        //   contexts.current[index]?.stroke()
-        //   console.log('123')
-        // }
       }
     });
   }, []);
@@ -58,39 +59,20 @@ const Canvas: React.FC<CanvasProps> = ({ size }) => {
     // console.log(centerX, centerY)
     const context = contexts.current[index]
     // console.log(context)
-    // if(context) {
-    //   context.strokeStyle = 'red'
-    //   context.beginPath();
-    //   context.moveTo(rect.width - 20, 0 + 20)
-    //   context.lineTo(0 + 20, rect.height - 20)
-    //   context.moveTo(0 + 20, 0 + 20)
-    //   context.lineTo(rect.width - 20, rect.height - 20)
-    //   context.stroke()
-    //   console.log('123')
-    // }
-    // setPass()
-    // setPass([...pass, context!.isPointInPath(centerX, centerY)])
-    
-    // setPass([pass[index] = passed])
-    
+  
     if(!pass[index]) {
-      // pass[index] = passed
-      if(isTurn) {
-        setIsTurn(false)
+      if(currentPlayer === 'o') {
         if(context) {
           context.beginPath();
           context.ellipse(centerX, centerY, 40, 55, 0, 0, 2 * Math.PI)
           context.strokeStyle = 'red'
           context.stroke()
           context.closePath()
-          // let passed = context!.isPointInPath(centerX, centerY)
           pass[index] = '0'
-          // console.log('isPoint', passed)
-          // console.log('passIndex', pass[index])
-          // setPass(pass.map((item, indexItem) => index === indexItem ? item = passed: item))
         }
+        setCurrentPlayer('x')
       } else {
-        setIsTurn(true)
+       
         if(context) {
           context.beginPath();
           context.moveTo(rect.width - 20, 0 + 20)
@@ -100,34 +82,36 @@ const Canvas: React.FC<CanvasProps> = ({ size }) => {
           context.strokeStyle = 'blue'
           context.stroke()
           context.closePath()
-          // let passed = context!.isPointInStroke(centerX, centerY)
           pass[index] = '1'
-          // console.log('isPoint', passed)
-          // console.log('passIndex', pass[index])
         }
+        setCurrentPlayer('o')
       }
     }
-    // console.log(matrixArray(pass, 3))
-    
+
+    if(checkWin(pass)) {
+      setModalMessage(`winner: ${currentPlayer}!!!`)
+      setGameActive(false)
+    } else if(checkDraw(pass)) {
+      setModalMessage('draw!')
+      setGameActive(false)
+    }
   }
-  console.log(matrixArray(pass, 3))
+
+  console.log('check win', checkWin(pass))
+  console.log('check draw', checkDraw(pass))
   console.log('passed', pass)
+
+  console.log('mess', modalMessage)
+  // console.log(!gameActive)
   return (
     <>
     <div className='canvas-wrapper'>
-      {/* <canvas ref={canvasRef} width={size} height={size}></canvas> */}
-      {/* <button>123</button> */}
-      {/* {[...Array(10)].map((x, i) => 
-      <div key={i}><button>123</button></div>
-      )} */}
+      {!gameActive && <Modal>{modalMessage}</Modal>}
       <div style={{width: size, height: size}}>
-        {/* {[...Array(3)].map((_, index) => (
-
-        ))} */}
         <div className='div-row' style={{width: widhtSize, height: size}}>
           {[...Array(9)].map((_, index) => (
-            <div key={index} className='btn-row' style={{width: size / 3, height: size / 3}}
-            onClick={(e: any) => drawing(e, index)}>
+            <div key={index} className={`btn-row`} style={{width: size / 3, height: size / 3}}
+            onClick={(e: any) => drawing(e, index)} aria-disabled={gameActive}>
               {/* {value === index + 1 ? value : null} */}
               <canvas
                 ref={(canvasRef) => {
